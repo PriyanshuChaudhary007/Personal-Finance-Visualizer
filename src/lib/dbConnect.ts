50a22471dc706bec29322e4ab4b3a2201a516df8
PriyanshuChaudhary007 <priyanshurana814@gmail.com>
2025-07-05 20:08:46 +0530

    Fix: Removed deprecated appDir config

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -1,39 +1,22 @@
+
+
+
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+let isConnected = false;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable');
-}
-
-declare global {
-  var mongoose: any;
-}
-
-let cached = global.mongoose || { conn: null, promise: null };
-
-export const connectDB = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      dbName: 'financevisualizer',
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
+const connectDB = async () => {
+  if (isConnected) return;
 
   try {
-    cached.conn = await cached.promise;
-  } catch (e) {
-    cached.promise = null;
-    throw e;
+    await mongoose.connect(process.env.MONGODB_URI as string, {
+      dbName: 'financevisualizer',
+    });
+    isConnected = true;
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
   }
-
-  return cached.conn;
 };
+
+export default connectDB;
